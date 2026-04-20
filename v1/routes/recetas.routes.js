2c33350a62f8065c7ef10620cc5eb9547753b3c0
Eduardo Monzón <xeriousuy@gmail.com>
2026-04-20 18:59:19 -0300

    refactor: eliminar middleware parseJsonFields de las rutas de recetas

--- v1/routes/recetas.routes.js
+++ v1/routes/recetas.routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { roleMiddleware } from "../middlewares/role.middleware.js";
 import { validateBodyMiddleware } from "../middlewares/validateBody.middleware.js";
 import { validateObjectIdMiddleware } from "../middlewares/validateObjectId.middleware.js";
-import { parseJsonFields } from "../middlewares/parseJsonFields.middleware.js";
 import { upload } from "../middlewares/upload.middleware.js";
 import {
   recetaSchema,
@@ -24,14 +23,12 @@ router.get("/", obtenerRecetas);
 router.get("/mis-recetas", obtenerMisRecetas);
 router.get("/:id", validateObjectIdMiddleware, obtenerRecetaPorId);
 
-
 // protegidas para chefs
 router.use(roleMiddleware("chef"));
 
 router.post(
   "/",
   upload.single("imagen"),
-  parseJsonFields(["ingredientes", "pasos"]),
   validateBodyMiddleware(recetaSchema),
   crearReceta,
 );
@@ -40,7 +37,6 @@ router.put(
   "/:id",
   validateObjectIdMiddleware,
   upload.single("imagen"),
-  parseJsonFields(["ingredientes", "pasos"]),
   validateBodyMiddleware(updateRecetaSchema),
   editarReceta,
 );
